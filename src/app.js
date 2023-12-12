e6ded506138c6664a0e332cad6bf97bfabe65419
cdavis2223 <cdavis2223@gmail.com>
2023-12-12 02:33:36 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function cargarPagina() {
+  setTimeout(function() {
+    generadorBoton();
+    cargarPagina();
+  }, 1000);
+}
+
+function generadorBoton() {
+  const icono = ["♦", "♥", "♠", "♣"];
+  const numero = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "As"];
+  const colores = ["#000000", "#ff0000"];
+
+  let simboloRandon = Math.floor(Math.random() * icono.length);
+  let numeroRandom = Math.floor(Math.random() * numero.length);
+  let colorRandom = Math.floor(Math.random() * colores.length);
+
+  document.querySelector("#numero").innerHTML = numero[numeroRandom];
+
+  document.querySelector("#simboloTop").innerHTML = icono[simboloRandon];
+  document.querySelector("#simboloBottom").innerHTML = icono[simboloRandon];
+
+  document.querySelector("#simboloBottom").style.color = colores[colorRandom];
+  document.querySelector("#simboloTop").style.color = colores[colorRandom];
+}
+
+window.onload = function aleatorio() {
+  generadorBoton();
+
+  document.querySelector("button").onclick = function() {
+    generadorBoton();
+  };
+
+  cargarPagina();
 };
